e7ee64f9b501c78e0e11573fad98c56a2b8c5709
Earth <63118195+EarthWittawat@users.noreply.github.com>
2025-03-09 17:44:30 +0700

    authentication demo

---
+++ auth.ts
@@ -0,0 +1,36 @@
+
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+import PostgresAdapter from "@auth/pg-adapter"
+import { Pool } from "pg"
+import Nodemailer from "next-auth/providers/nodemailer"
+
+const pool = new Pool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
+
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+ adapter: PostgresAdapter(pool),
+  providers: [
+    Google,
+    Nodemailer({
+        server: {
+          host: process.env.EMAIL_SERVER_HOST,
+          port: process.env.EMAIL_SERVER_PORT,
+          auth: {
+            user: process.env.EMAIL_SERVER_USER,
+            pass: process.env.EMAIL_SERVER_PASSWORD,
+          },
+        },
+        from: process.env.EMAIL_FROM,
+      }),
+]
+  ,
+})
