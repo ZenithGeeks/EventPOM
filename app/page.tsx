6af34571b85328b9fd949e2e2e3a6e5c25791392
Earth <63118195+EarthWittawat@users.noreply.github.com>
2025-03-09 15:55:57 +0700

    general navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
-import FetchComponent from "@/app/components/fetch";
 
 export default function Page()  {
   return (
     <div className="bg-white min-w-screen min-h-screen flex items-center justify-center">
-      <FetchComponent />
 
     </div>
   );
