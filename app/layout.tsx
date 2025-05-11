5ceda2277eae7f9a46d62acff643e47952384399
Gunnkaruntancharoen <98071227+Ejokr@users.noreply.github.com>
2025-05-11 13:15:46 +0700

    Linked to backend.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,32 +3,34 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import { SessionProvider } from "next-auth/react"
+import { Toaster } from "react-hot-toast";
 
-const Poppinsfont = Poppins({subsets: ["latin"], weight: "500"});
+const Poppinsfont = Poppins({ subsets: ["latin"], weight: "500" });
 
 export const metadata: Metadata = {
-  title: "EventPOM",
-  description: "EventPOM is a platform for managing events",
-  viewport: "width=device-width, initial-scale=1",
-  keywords: ["EventPOM", "events", "management"],
+    title: "EventPOM",
+    description: "EventPOM is a platform for managing events",
+    viewport: "width=device-width, initial-scale=1",
+    keywords: ["EventPOM", "events", "management"],
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${Poppinsfont} antialiased`}
-      >
-         <SessionProvider >
-              <Navbar />
-        {children}
-         </SessionProvider>
-    
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${Poppinsfont} antialiased`}
+            >
+                <SessionProvider >
+                    <Toaster />
+                    <Navbar />
+                    {children}
+                </SessionProvider>
+
+            </body>
+        </html>
+    );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
-        search: '',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+                search: '',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '9000',
+                pathname: '/**'
+            }
+        ],
+    },
 };
 
 export default nextConfig;
